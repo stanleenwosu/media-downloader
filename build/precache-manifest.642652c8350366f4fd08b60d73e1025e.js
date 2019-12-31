c0b8ee46c5eb19005e6c682e8f7ef0deb20f951f
stanleenwosu <stanleenwosu@yahoo.com>
2019-12-31 22:27:16 +0100

    Improved the UI

---
+++ build/precache-manifest.642652c8350366f4fd08b60d73e1025e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0bc23caa688c0ed3a25306ee0186c3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b113fee0a95e7f504533",
+    "url": "/static/css/main.ff0a73ce.chunk.css"
+  },
+  {
+    "revision": "68fd3c203d8e26db663d",
+    "url": "/static/js/2.339144ff.chunk.js"
+  },
+  {
+    "revision": "b113fee0a95e7f504533",
+    "url": "/static/js/main.91d7a41e.chunk.js"
+  },
+  {
+    "revision": "02145b34d99503519d88",
+    "url": "/static/js/runtime-main.8c1fb3c8.js"
+  }
+]);
