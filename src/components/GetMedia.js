36eb02db619f79386c3a996ef501174e223e351e
stanlee20 <stanleenwosu@gmail.com>
2021-01-27 13:07:49 +0100

    UI Fix

--- src/components/GetMedia.js
+++ src/components/GetMedia.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import MediaThumbnail from "./MediaThumbnail";
 import VideoList from "./VideoList";
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 
 class VideoList extends React.Component {
